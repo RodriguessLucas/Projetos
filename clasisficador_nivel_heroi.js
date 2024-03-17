8001c3a97a81cd7f066487c3fac0d0e572c5b130
Lucas Rodrigues Pereira <158780106+RodriguessLucas@users.noreply.github.com>
2024-03-17 08:28:28 -0300

    Add files via upload

---
+++ clasisficador_nivel_heroi.js
@@ -0,0 +1,29 @@
+let nome_herio = "Ramises";
+let pontuacao  = 5500;
+
+if (pontuacao >=1001 && pontuacao <=2000){
+    console.log("O héroi", nome_herio, "está no Bronze.");
+} 
+else if (pontuacao >=2001 && pontuacao <=5000){
+    console.log("O héroi", nome_herio, "está no Prata.");
+}
+else if (pontuacao >=5001 && pontuacao <=7000){
+    console.log("O héroi", nome_herio, "está no Ouro.");
+}
+else if (pontuacao >=7001 && pontuacao <=8000){
+    console.log("O héroi", nome_herio, "está no Platina.");
+}
+else if (pontuacao >=8001 && pontuacao <=9000){
+    console.log("O héroi", nome_herio, "está no Ascendente.");
+}
+else if (pontuacao >=9001 && pontuacao <=10000){
+    console.log("O héroi", nome_herio, "está no Imortal.");
+}
+else if (pontuacao >=10001){
+    console.log("O héroi", nome_herio, "está no Radiante.");
+}
+else{
+    console.log("O héroi não tem pontuação suficiente.")
+}
+
+
